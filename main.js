24bdf3dc593a98f184f74a7de6e64eed869553d7
Huy Nguyen <ngochuy@dropbox.com>
2016-01-25 10:24:35 -0800

    Sample form dialog

--- main.js
+++ main.js
@@ -1,29 +1,14 @@
 var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
-var applescript = require('applescript');
-var tmp = require('temporary');
+var ipc=require('node-ipc');
 
-// Helper function to download picture from url and set it as wallpaper
-exports.setWallpaper = function(url) {
-  var dir = new tmp.Dir();
-  var path = dir.path + '/wallpaper.pic';
-  // Applescript to download the wallpaper to a temp file and set it as
-  // wallpaper
-  var script =
-    'do shell script "curl -L ' + url + ' -o ' + path + '"\n' +
-    'tell application "System Events"\n' +
-    '  tell current desktop\n' +
-    '    set picture to "' + path + '"\n' +
-    '  end tell\n' +
-    'end tell\n';
-  applescript.execString(script, function(err, rtn) {
-    if (err) {
-      // Something went wrong!
-      console.log("Something went wrong");
-    }
-  });
+const ipcMain = require('electron').ipcMain;
 
-}
+ipcMain.on('sendInvite', function(event, arg) {
+  console.log('sendInvite args: ', arg);
+  ipc.server.emit(ipc.server.current_socket, arg);
+});
+
 
 // Report crashes to our server.
 require('crash-reporter').start();
@@ -31,6 +16,8 @@ require('crash-reporter').start();
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
+
+app.dock.hide();
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
@@ -44,25 +31,64 @@ app.on('window-all-closed', function() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    title: 'Wallpaper'
-  });
 
-  // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
-  // mainWindow.loadUrl('https://mail.google.com/');
+  /***************************************\
+   *
+   * You should start both hello and world
+   * then you will see them communicating.
+   *
+   * *************************************/
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
+  ipc.config.id   = 'test';
+  ipc.config.retry= 1500;
+  ipc.config.rawBuffer=true;
+  ipc.config.encoding='ascii';
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+  ipc.serve(
+      function(){
+          ipc.server.on(
+              'connect',
+              function(socket){
+                  ipc.server.current_socket = socket;
+                  ipc.server.emit(
+                      socket,
+                      'hello'
+                  );
+              }
+          );
+
+          ipc.server.on(
+              'data',
+              function(data,socket){
+                  ipc.log('got a message'.debug, data,data.toString());
+                  // Create the browser window.
+                  mainWindow = new BrowserWindow({
+                    width: 800,
+                    height: 600,
+                    title: 'Wallpaper'
+                  });
+
+                  // and load the index.html of the app.
+                  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+                  // Open the DevTools.
+                  mainWindow.webContents.openDevTools();
+
+                  // Emitted when the window is closed.
+                  mainWindow.on('closed', function() {
+                    // Dereference the window object, usually you would store windows
+                    // in an array if your app supports multi windows, this is the time
+                    // when you should delete the corresponding element.
+                    mainWindow = null;
+                  });
+                  ipc.server.emit(
+                      socket,
+                      'goodbye'
+                  );
+              }
+          );
+      }
+  );
+
+  ipc.server.start();
 });
--- src/app.js
+++ src/app.js
@@ -1,82 +1,28 @@
 'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
-import Lightbox from './react-lightbox.js';
 
 const App = React.createClass({
-  // API template to get newest wallpaper from alphacoders.com
-  source: "http://wall.alphacoders.com/api1.0/get.php?auth=9d1d1f1bb35191348e2c54556dd4bf62",
-  getInitialState() {
-    return {
-      // Images info (including url, thumbnail and title).
-      images: [],
-      lightboxIsOpen: false,
-    };
+  getInitialState: function() {
+    return {value: ''};
   },
-  // Preload images in background to avoid delay when user click on thumbnail
-  preloadImages: [],
-  preloadImages: function() {
-    $.each(this.state.images, function(i, image) { $.get(image.link); });
+  handleInputChange: function(event) {
+    this.setState({value: event.target.value});
   },
-  queryWallpapers: function(page) {
-    // There are 30 wallpapers on each page
-    var url = this.source + "&page=" + page;
-    // AJAX request for wallpapers
-    $.get(url, function(result) {
-      var items = [];
-      for (var i = 0; i < result.wallpapers.length; i++) {
-        items.push({
-          link: result.wallpapers[i].url,
-          thumbnailLink: result.wallpapers[i].thumb.url,
-          // The wallpaper info from this API does not have title. Use combination of
-          // category and sub_category as title (for this demo).
-          title: result.wallpapers[i].category + ' - ' + result.wallpapers[i].sub_category
-        });
-      }
-      if (this.isMounted()) {
-        this.setState({
-          images: this.state.images.concat(items),
-          lightboxIsOpen: true,
-        });
-      }
-      // Continue query for more wallpapers until we have at least 100.
-      if (this.state.images.length < 100) {
-        this.queryWallpapers(page + 1);
-      } else {
-        // Wait for 1 sec (so that the thumbnail will be loaded before preloading the
-        // real wallpaper).
-        setTimeout(this.preloadImages, 1000);
-      }
-    }.bind(this));
-  },
-  componentDidMount: function () {
-    this.queryWallpapers(1);
-  },
-  onWallpaperSelected: function(index) {
-    if (index < 0 || index > this.state.images.length) {
-      console.log("Index out of range: ", index);
-      return;
-    }
-    // Communicate to the main process (via remote) to set desktop wallpaper to the
-    // given url.
-    var url = this.state.images[index].link;
-    setWallpaper(url);
+  onButtonClick: function(event) {
+    ipcRenderer.send('sendInvite', this.state.value);
   },
   render: function() {
     return (
       <div>
-        <Lightbox
-          images={this.state.images}
-          keyboard
-          onWallpaperSelected={this.onWallpaperSelected}
-        />
+        Who do you want to share this folder with?
+        <input type="text" onChange={this.handleInputChange}/>
+        <button type="button" onClick={this.onButtonClick}>Share</button>
       </div>
     );
   }
 
 });
-
 
 (function() {
   ReactDOM.render(<App/>, document.getElementById('react-root'));
